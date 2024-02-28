d81b5f15747b252c9f57c9fa4795b03f5026488b
abhijithchowdary2 <abhijithchowdary2@gmail.com>
2024-02-28 10:03:13 +0530

    Manual build from jenkins after removing triggers

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
     public static void main(String[] args){
         for(int i=1;i<=10;i++){
-            System.out.println("Calling abhijith "+ i +" time(s)");
+            // System.out.println("Calling abhijith "+ i +" time(s)");
+            System.out.println("The value of i is "+ i);
         }
     }
 }
