2db1adb1c5ede5855600afd8ba6a242022e4fa08
abhijithchowdary2 <abhijithchowdary2@gmail.com>
2024-02-28 10:45:43 +0530

    for catlight notification

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 public class Hello{
     public static void main(String[] args){
-        for(int i=1;i<=10;i++){
-            // System.out.println("Calling abhijith "+ i +" time(s)");
-            System.out.println("The value of i is "+ i);
-        }
+        // for(int i=1;i<=10;i++){
+        //     // System.out.println("Calling abhijith "+ i +" time(s)");
+        //     System.out.println("The value of i is "+ i);
+        // }
+        System.out.println("Abhijith Chowdary");
     }
 }
