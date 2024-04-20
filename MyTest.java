ea3e148cb08dfc8670bafb176d1ef1a4538287af
abhijithchowdary2 <abhijithchowdary2@gmail.com>
2024-04-20 19:40:50 +0530

    Changes in MyTest.java file

--- MyTest.java
+++ MyTest.java
@@ -1,7 +1,7 @@
 class MyTest {
 	public static void main(String args[]){
 		for(int i = 1; i <= 10; i++){
-			System.out.println(i + " on 20th April 2024");
+			System.out.println(i + " on 20th April 24");
 		}
 	}
 }
